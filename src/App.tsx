4b3058d9bd67a949a54aa568662b890b3c716da9
FernandoNunesRizzato <fernandorizzato@outlook.com>
2021-10-05 11:26:18 -0300

    Função de adicionar implementada

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item'
 import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea'
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Comprar o pão', done: false},
-    { id: 2, name: 'Comprar um bolo', done: false},
+    
  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
 
 
   return (
@@ -16,6 +26,7 @@ const App = () => {
         <C.Header>Lista de Tarefas</C.Header>
 
           {/*Área para adicionar nova tarefa */}
+          <AddArea onEnter={handleAddTask} />
 
           {/*Lista de tarefas */}
           {list.map((item, index)=>(
