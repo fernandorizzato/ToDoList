7f57f0a23704be540758effc42ce81f688e91512
FernandoNunesRizzato <fernandorizzato@outlook.com>
2021-10-04 21:54:11 -0300

    checkbox de marcação implementado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão', done: false},
+    { id: 2, name: 'Comprar um bolo', done: false},
+ ]);
+
+
   return (
     <C.Container>
       <C.Area>
-        <C.Header>
-Lista de Tarefas
-        </C.Header>
+        <C.Header>Lista de Tarefas</C.Header>
+
+          {/*Área para adicionar nova tarefa */}
+
+          {/*Lista de tarefas */}
+          {list.map((item, index)=>(
+            <ListItem key={index} item={item} />              
+          ))}
+       
       </C.Area>
     </C.Container>
   );
